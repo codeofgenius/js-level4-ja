8cb12f415bc8bfa003ceba2148658ab586446d79
Hiroshi Fujiwara <hiroshi.829f@gmail.com>
2026-01-28 07:55:22 +0000

    added base file

---
+++ chapter04/work10.js
@@ -0,0 +1,13 @@
+// Setをfor-of文で利用
+// 空のSetの作成
+const fruits = new Set();
+
+// 要素の追加
+fruits.add('Apple');
+fruits.add('Orange');
+fruits.add('Melon');
+
+// Setをfor-of文で繰り返し処理
+for( const value of fruits ) {
+    console.log( value );
+}
